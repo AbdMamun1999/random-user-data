f4e50a94d6aea5bd2b833c80b041ee77ab4c4157
Abdullah Al Mamun <mamun12121999@gmail.com>
2022-09-03 14:03:24 +0600

    set an api for save a user in json file

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,24 +1,11 @@
 const fs = require("fs");
 
-/* let allUserJson = () =>
-  fs.readFile("./users.json", "utf8", (err, data) => {
-    if (err) {
-      res.write("failed to read data");
-      res.end();
-    } else {
-      res.write(data);
-      res.end();
-    }
-  }); */
-
 module.exports.getRandomUser = (req, res) => {
   fs.readFile("./users.json", "utf8", (err, data) => {
     if (err) {
       res.write("failed to write");
     } else {
       const allUser = JSON.parse(data);
-      // res.write(allUser)
-      // res.end()
       const randomUser = allUser[Math.floor(Math.random() * allUser.length)];
       res.send(randomUser);
     }
@@ -38,5 +25,24 @@ module.exports.getAllUser = (req, res) => {
 };
 
 module.exports.saveAUser = (req, res) => {
-  console.log("save a data");
+  const newData = req.body;
+  const allUserJson = fs.readFileSync("users.json");
+  let allUsers = JSON.parse(allUserJson);
+  allUsers.push(newData);
+
+  const allUsersStringfy = JSON.stringify(allUsers);
+  fs.writeFile("users.json", allUsersStringfy, (err) => {
+    if (err) {
+      res.write("Data failed to save");
+      res.end();
+    } else {
+      res.write("save successfully");
+      res.end();
+    }
+  });
 };
+
+/* module.exports.updateAUser = (req,res)=>{
+   const newData = req.body;
+   console.log(newData)
+} */
